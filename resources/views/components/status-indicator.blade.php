862a86e84ddb07f078a0697bccdec3730a56d584
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-03-18 19:45:21 -0500

    doing little change in status indicator

--- resources/views/components/status-indicator.blade.php
+++ resources/views/components/status-indicator.blade.php
@@ -1,16 +1,22 @@
 <button type="submit" data-bs-toggle="dropdown" aria-expanded="false" class="navbar-toggler" data-bs-auto-close="outside">
-        <div class="flex max-md:flex-col items-center justify-center gap-2 hover:bg-blue-200 p-2 rounded transition-all duration-300">
-                <span>
-                        @if ($status === 'completed') Completed
-                        @elseif ($status === 'in-progress') In progress
-                        @elseif ($status === 'canceled') Canceled
-                        @else Not Start @endif
-                </span>
-                <span class="h-2 w-2 rounded-full
-                                @if($status === 'completed') bg-success
-                                @elseif($status === 'in-progress') bg-warning
-                                @elseif($status === 'canceled') bg-danger
-                                @else bg-secondary @endif">
-                </span>
-        </div>
+    <div class="flex max-md:flex-col items-center justify-center gap-2 hover:bg-blue-200 p-2 rounded transition-all duration-300">
+        <?php
+            $statusText = match($status) {
+                'completed' => 'Completed',
+                'in-progress' => 'In progress',
+                'canceled' => 'Canceled',
+                default => 'Not Start'
+            };
+
+            $statusColor = match($status) {
+                'completed' => 'bg-success',
+                'in-progress' => 'bg-warning',
+                'canceled' => 'bg-danger',
+                default => 'bg-secondary'
+            };
+        ?>
+
+        <span>{{ $statusText }}</span>
+        <span class="h-2 w-2 rounded-full {{ $statusColor }}"></span>
+    </div>
 </button>
